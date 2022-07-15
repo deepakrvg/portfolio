8f46f9dde952b83653a8041d0f0d0d25e0d7951a
deepakrvg <guptadeepak0908@gmail.com>
2022-07-15 19:00:54 +0530

    added floating navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
 
 const Nav = () => {
     return (
         <nav>
-            <a href="#"><AiOutlineHome /></a>
+            <a href="#home" className="active"><AiOutlineHome /></a>
+            <a href="#about"><AiOutlineUser /></a>
+            <a href="#experience"><BiBook /></a>
+            <a href="#services"><RiServiceLine /></a>
+            <a href="#contact"><BiMessageSquareDetail /></a>
         </nav>
     )
 }
